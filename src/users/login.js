3cbb5dcddb2d4d63d212741e54c6c21be8a9ad89
7sy3 <89568830+7sy3@users.noreply.github.com>
2022-12-05 19:55:01 -0800

    update for login

--- src/users/login.js
+++ src/users/login.js
@@ -6,40 +6,21 @@ import {Navigate, useNavigate} from "react-router";
 const Login = () => {
     const [username, setUsername] = useState('alice')
     const [password, setPassword] = useState('alice123')
-   /* const [validatePassword, setValidatePassword] = useState('alice123')*/
+
     const [error, setError] = useState(null)
     const dispatch = useDispatch()
-    const navigate = useNavigate()
     const { currentUser } = useSelector((state) => state.users)
-/*    const handleLoginBtn = () => {
-        if (password !== validatePassword) {
-            setError('Passwords must match')
-            return
-        }
-        setError(null)
-        // const newUser = {
-        //     username: username,
-        //     password: password
-        // }
-        const loginUser = {
-            username, password
-        }
-        dispatch(loginThunk(loginUser))
-    }*/
+
     const handleLoginBtn = () => {
         try {
             dispatch(loginThunk({username, password}))
-            // navigate('/profile')
         } catch (e) {
-
         }
     }
     if (currentUser) {
         return (<Navigate to={'/profile'}/>)
     }
-    if (currentUser) {
-        return (<Navigate to={'/profile'}/>)
-    }
+
     return (
         <>
             <h1>Login</h1>
